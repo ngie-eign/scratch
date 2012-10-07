f069242ff05bec8ece541be3a98bfe505d6d02ab
Garrett Cooper <yanegomi@gmail.com>
2012-10-07 07:43:04 -0700

    Polish it a bit..
    Sponsored-by: EMC Corporation
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- tahi/scripts/run_tahi.sh
+++ tahi/scripts/run_tahi.sh
@@ -3,22 +3,54 @@
 # Run the TAHI p1/p2 tests in sequence. Nothing special here; just some simple
 # canned commands.
 
-set -e
+# Check to make sure the directories can be shuffled, run the tests, and
+# shuffle the results to the right directory once done.
+#
+# Parameters:
+# 1 - phase
+# 2 - type (host, router, special)
+run()
+{
+	local phase=$1
+	local type=$2
 
-if [ ! -f "Makefile.test" ]
+	# Phase directory.
+	pdir=../$phase
+	# Prereq checks (see if the directory is writable, etc).
+	set -e
+	[ -d $pdir ] || mkdir $pdir # Make sure the directory exists.
+	: > $pdir/.w # Truncate a file in the directory (writable).
+	rm $pdir/.w # Clean it up after we're done.
+	set +e
+	# Run the test.
+	make ipv6ready_${phase}_${type}
+	# Shuffle the results around.
+	rm -Rf $pdir
+	cp -Rf . $pdir
+}
+
+if ! make -f Makefile.test ipv6ready_p1_host >/dev/null 2>&1
 then
-	echo >&2 "${0##*/}: not in the Self_Test directory"
+	echo >&2 "${0##*/}: this must be run from Self_Test directory"
 	exit 1
 fi
 
-run()
-{
-	[ -d ../$1 ] || mkdir ../$1
-	: > ../$1/.w
-	rm ../$1/.w
-	make ipv6ready_${1}_host || :
-	rm -Rf ../$1
-	cp -Rf . ../$1
-}
-run "p1"
-run "p2"
+case "$1" in
+host|router|special)
+	type=$1
+	;;
+*)
+	cat >&2 <<EOF
+usage: ${0##*/}: [host|router|special]
+
+Defaults to host.
+EOF
+	exit 1
+	;;
+esac
+
+run "p1" "$type"
+if [ "$type" != "special" ]
+then
+	run "p2" "$type"
+fi
