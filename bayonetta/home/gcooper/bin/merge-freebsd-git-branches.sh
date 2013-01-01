0e7ecc19d2ea2552cc324da326a6147cd1be34a2
Garrett Cooper <yanegomi@gmail.com>
2013-01-01 11:02:14 -0800

    Add script for merging branches in my github repo
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

---
+++ bayonetta/home/gcooper/bin/merge-freebsd-git-branches.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+# Avoid the glob ;).
+git checkout master
+branches=$(git branch -l | grep -v master)
+for branch in $branches
+do
+	case "$branch" in
+	stable*)
+		parent_branch=origin/$branch
+		;;
+	*)
+		parent_branch=master
+		;;
+	esac
+	git checkout $branch && git merge $parent_branch || exit 1
+done
+if [ -n "$branches" ]
+then
+	git push --all
+fi
