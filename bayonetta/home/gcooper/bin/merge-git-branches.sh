db51b73d1198db92637f984a2949477e2ec5ceaf
Garrett Cooper <yanegomi@gmail.com>
2013-07-30 00:30:08 -0700

    Add merge support for stable branches
    Merge stable-foo-bar from stable/foo
    
    Sort the branches so stable/foo comes before stable-foo

--- bayonetta/home/gcooper/bin/merge-git-branches.sh
+++ bayonetta/home/gcooper/bin/merge-git-branches.sh
@@ -5,12 +5,15 @@ set -e
 git checkout master
 git pull
 git pu
-branches=$(git branch -l | grep -v master || :)
+branches=$(git branch -l | grep -v master | sort -u || :)
 for branch in $branches
 do
 	case "$branch" in
-	stable*)
+	stable/*)
 		parent_branch=upstream/$branch
+		;;
+	stable-*)
+		parent_branch=$(echo $branch | sed -e 's,stable-,stable ,g' -e 's,-.*,,g' -e 's,stable ,stable/,g')
 		;;
 	*)
 		parent_branch=master
