4720adfda243d26e4b60fa713d6b5e88bdeafe09
Garrett Cooper <yanegomi@gmail.com>
2013-10-24 16:11:43 -0700

    Remove -j24 hardcoding
    I'm not going to be running this on systems that don't scale
    well on FreeBSD for a while (SNB+).
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- bayonetta/root/bin/build.sh
+++ bayonetta/root/bin/build.sh
@@ -3,8 +3,8 @@
 set -ex
 : ${JOBS=$(sysctl -n kern.smp.cpus)}
 rm -f ~/bw.s
-(make buildworld -j24 2>&1 && touch ~/bw.s) | tee ~/bw.log
+(make buildworld -j$JOBS 2>&1 && touch ~/bw.s) | tee ~/bw.log
 [ -f ~/bw.s ]
 rm -f ~/bk.s
-(make buildkernel -j24 2>&1 && touch ~/bk.s) | tee ~/bk.log
+(make buildkernel -j$JOBS 2>&1 && touch ~/bk.s) | tee ~/bk.log
 [ -f ~/bk.s ]
