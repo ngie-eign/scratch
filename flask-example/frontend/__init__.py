cead8aae32efbb98c07fd4c3dd07c43dfb07ce81
Garrett Cooper <yanegomi@gmail.com>
2013-10-30 12:30:28 -0700

    NULL out __license__
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- flask-example/frontend/__init__.py
+++ flask-example/frontend/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = "Garrett Cooper <garrett.cooper@zonarsystems.com>"
 __date__ = "$Date$"
-__license__ = "Copyright Only"
+__license__ = ""
 __version_info__ = (0, 0, 1)
 __version__ = '.'.join(map(str, __version_info__))
