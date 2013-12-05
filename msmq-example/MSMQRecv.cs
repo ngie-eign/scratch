f9121066aba03cd398a7c62bb208715da43607fa
Garrett Cooper <yanegomi@gmail.com>
2013-12-05 10:45:23 -0800

    Examples for how to use MSMQ
    Sponsored-by: Zonar Systems, Inc

---
+++ msmq-example/MSMQRecv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+using Messaging = System.Messaging;
+
+namespace MSMQRecv
+{
+    class MSMQRecv
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args) {
+                MessageQueue queue = new MessageQueue(arg);
+                try
+                {
+                    foreach (Message msg in queue.GetAllMessages())
+                    {
+                        msg.Formatter = new XmlMessageFormatter(new Type[1] { typeof(string) });
+                        Console.Write("Received message (" + msg.Label + ") with payload: `" + msg.Body + "` from queue " + arg + "\n");
+                    }
+                } finally {
+                    queue.Close();
+                }
+            }
+        }
+    }
+}
---
+++ msmq-example/MSMQSend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+using Messaging = System.Messaging;
+
+namespace MSMQSend
+{
+    class MSMQSend
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                MessageQueue queue = new MessageQueue(arg);
+                try
+                {
+                    Message msg = new Message();
+                    msg.Body = "Hello, the time is: " + System.DateTime.Now;
+                    msg.Label = "Test Message " + msg.Body.GetHashCode();
+                    Console.Write("Will send message " + msg.Label + "  `" + msg.Body + "` to queue: " + arg + "\n");
+                    if (queue.Transactional)
+                        queue.Send(msg, MessageQueueTransactionType.Single);
+                    else
+                        queue.Send(msg);
+                }
+                finally
+                {
+                    queue.Close();
+                }
+            }
+            Console.Write("Press enter to continue..");
+            Console.Read();
+        }
+    }
+}
