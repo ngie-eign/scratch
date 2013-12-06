b085f1f305a7575739dde23706998280db1573d9
Garrett Cooper <yanegomi@gmail.com>
2013-12-05 19:28:56 -0800

    Handle programmatic creation of queues more gracefully

--- msmq-example/MSMQSend.cs
+++ msmq-example/MSMQSend.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Messaging;
 using Messaging = System.Messaging;
 
@@ -12,36 +13,73 @@ namespace MSMQSend
     {
         static void Main(string[] args)
         {
+            MessageQueueTransaction transaction;
+            //const string direct_format = "formatname:direct";
+
             // XXX: add a usage message.
             // `.\private$\test`,
             // `FormatName:DIRECT=HTTP://localhost/msmq/private$/test`,
             // etc
             foreach (string arg in args)
             {
-                Boolean isHTTP = false;
+                // Best guesses for the magic tables in the MSDN docs for
+                // Local, Remote, etc.
+                //Boolean isDirect = Regex.IsMatch(arg, "^" + Regex.Escape(direct_format) + "=");
+                Boolean isLocal = Regex.IsMatch(arg, "^" + Regex.Escape(".\\"));
 
+                // Create a private queue if it's being created on the local
+                // machine.
                 // http://stackoverflow.com/questions/5559123/programmatically-add-private-queues-in-msmq
-                if (arg.ToLower().IndexOf("FormatName:DIRECT=HTTP://") != -1)
+                if (isLocal && !MessageQueue.Exists(arg))
                 {
-                    isHTTP = true;
-                }
-
-                if (!isHTTP && !MessageQueue.Exists(arg)) {
                     MessageQueue.Create(arg, true);
                 }
+
                 MessageQueue queue = new MessageQueue(arg);
+                // Only local queues can be Transactional.
+                // http://msdn.microsoft.com/en-us/library/system.messaging.messagequeue.transactional%28v=vs.110%29.aspx
+                Boolean isTransactional = isLocal && queue.Transactional;
+
+                if (isTransactional)
+                {
+                    transaction = new MessageQueueTransaction();
+                }
+                else
+                {
+                    transaction = null;
+                }
+
+                // Use reliable messaging (i.e. store message on disk).
+                queue.DefaultPropertiesToSend.Recoverable = true;
+
                 try
                 {
                     Message msg = new Message();
+
                     msg.Body = "Hello, the time is: " + System.DateTime.Now;
                     msg.Label = "Test Message " + msg.Body.GetHashCode();
-                    Console.Write("Will send message " + msg.Label + "  `" + msg.Body + "` to queue: " + arg + "\n");
 
-                    if (!isHTTP && queue.Transactional)
+                    Console.WriteLine("Will send {0}transactional message " +
+                                      "({1}) with payload ({2}) to queue {3}",
+                                      (isTransactional ? "" : "non-"),
+                                      msg.Label, msg.Body, arg);
+
+                    // Send a transactional message. See also:
+                    // http://msdn.microsoft.com/en-us/library/3hyd9xby%28v=vs.110%29.aspx
+                    // http://msdn.microsoft.com/en-us/library/ms978430.aspx
+                    if (isTransactional)
                     {
-                        // Send a single, transactional, internal message. See also:
-                        // http://msdn.microsoft.com/en-us/library/3hyd9xby%28v=vs.110%29.aspx
-                        queue.Send(msg, MessageQueueTransactionType.Single);
+                        transaction.Begin();
+                        try
+                        {
+                            queue.Send(msg, transaction);
+                            transaction.Commit();
+                        }
+                        catch
+                        {
+                            transaction.Abort();
+                            throw;
+                        }
                     }
                     else
                     {
