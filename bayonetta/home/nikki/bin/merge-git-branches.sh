6bfa392da1764f2293a449d1b4daba745242ff86
garrett.cooper@zonarsystems.com <Garrett Cooper>
2014-02-12 10:29:46 -0800

    MASTER -> GIT_MASTER

--- bayonetta/home/nikki/bin/merge-git-branches.sh
+++ bayonetta/home/nikki/bin/merge-git-branches.sh
@@ -5,10 +5,10 @@ set -e
 original_branch=$(git branch -l | awk '$1 == "*" { print $NF }')
 trap "git checkout $original_branch" EXIT
 cd "$(git rev-parse --show-toplevel)"
-MASTER=master
-git checkout $MASTER
+GIT_MASTER=master
+git checkout $GIT_MASTER
 git pull --all
-branches=$(git branch -l | grep -v $MASTER | sort -du || :)
+branches=$(git branch -l | grep -v $GIT_MASTER | sort -du || :)
 for branch in $branches
 do
 	case "$branch" in
@@ -19,7 +19,7 @@ do
 		parent_branch=$(echo $branch | sed -e 's,stable-,stable ,g' -e 's,-.*,,g' -e 's,stable ,stable/,g')
 		;;
 	*)
-		parent_branch=$MASTER
+		parent_branch=$GIT_MASTER
 		;;
 	esac
 	git checkout $branch &&	git merge --no-edit $parent_branch || exit 1
