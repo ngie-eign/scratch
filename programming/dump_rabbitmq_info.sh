202db6dced60f199aac9c7123ca960abfcef39c2
garrett.cooper@zonarsystems.com <Garrett Cooper>
2014-02-27 19:41:27 -0800

    Dump the arguments for the list_bindings call as I can never remember the exact list and rabbitmqctl does not print out the columns when it dumps out the info (*sigh*)

--- programming/dump_rabbitmq_info.sh
+++ programming/dump_rabbitmq_info.sh
@@ -4,7 +4,9 @@ while :
 do
 	rabbitmqctl list_exchanges $*
 	rabbitmqctl list_queues $*
+	set -x
 	rabbitmqctl list_bindings $* source_name source_kind destination_name destination_kind routing_key
+	set +x
 	sleep 10
 	clear
 done
