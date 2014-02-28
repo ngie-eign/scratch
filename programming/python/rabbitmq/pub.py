e97202e91cfd5ebee0541ed8ef7c6ed2e6c6be46
garrett.cooper@zonarsystems.com <Garrett Cooper>
2014-02-28 11:00:19 -0800

    Simplify the publisher example

--- programming/python/rabbitmq/pub.py
+++ programming/python/rabbitmq/pub.py
@@ -3,11 +3,9 @@
 from pubsubcommon import *
 
 with kombu.Connection(**conn_dict) as conn:
-    producer = conn.Producer()
-    producer.publish(sys.stdin.read(),
-                     exchange=exchange,
-                     # XXX: why does this need to be here, i.e. why can't it
-                     # be obtained from the exchange object?
-                     routing_key=routing_key,
-                     declare=[queue],
-                     )
+    with conn.Producer(exchange=self.exchange,
+                       routing_key=queue.routing_key,
+                       ) as producer:
+        producer.publish(sys.stdin.read(),
+                         declare=[queue],
+                         )
