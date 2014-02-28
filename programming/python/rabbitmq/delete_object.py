12b68a05abcae8b6a03356804438ea1b3ae4dcc6
garrett.cooper@zonarsystems.com <Garrett Cooper>
2014-02-27 18:51:12 -0800

    Use argparse instead of a magic set of positional arguments

--- programming/python/rabbitmq/delete_object.py
+++ programming/python/rabbitmq/delete_object.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 import os
 import sys
 
 import kombu
 
-def usage():
-    sys.exit('usage: %s [exchange|queue] user password virtual-host'
-             % (os.path.basename(sys.argv[0]), ))
-
-
-if len(sys.argv) != 6:
-    usage()
-
-objname = sys.argv[1].capitalize()
-if objname not in ('Exchange', 'Queue', ):
-    usage()
+parser = ArgumentParser()
+parser.add_argument('--name',
+                    )
+parser.add_argument('--password',
+                    )
+parser.add_argument('--type',
+                    choices=('Exchange', 'Queue'),
+                    required=True,
+                    )
+parser.add_argument('--user',
+                    )
+parser.add_argument('--virtual-host',
+                    )
+args = parser.parse_args()
 
 conn_dict = {
     'hostname': 'localhost',
-    'userid': sys.argv[2],
-    'password': sys.argv[3],
-    'virtual_host': sys.argv[4],
+    'userid': args.user,
+    'password': args.password,
+    'virtual_host': args.virtual_host,
     'transport': 'pyamqp',
 }
-name = sys.argv[5]
+name = args.name
 
 with kombu.Connection(**conn_dict) as conn:
-    obj = getattr(kombu, objname)(name, channel=conn)
+    obj = getattr(kombu, args.type)(name, channel=conn)
     obj.delete()
