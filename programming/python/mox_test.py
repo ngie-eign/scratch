ede0a70c054e1ae5c5093df588309b95c552c1f4
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-04-03 16:55:51 -0700

    Add an example for how to use mox

---
+++ programming/python/mox_test.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+"""
+A complex-ish mox test example.
+
+.. moduleauthor: Garrett Cooper
+.. date: April 2014
+"""
+
+import sqlite3
+import unittest
+
+import mox
+
+from nose.tools import (
+     assert_raises,
+     )
+
+tmp_database = None
+
+def setup():
+    """Initialize an sqlite3 object in memory with a dummy table"""
+
+    global tmp_database
+
+    tmp_database = sqlite3.connect(':memory:')
+    table_create_sql = \
+        'CREATE TABLE ' \
+        'perple_table(name STRING, age INTEGER, PRIMARY KEY(name ASC));'
+    tmp_database.execute(table_create_sql)
+
+
+def teardown():
+    """Not strictly required, but close the db object when the test is done"""
+
+    tmp_database.close()
+
+
+class BadInsert(Exception):
+    """Exception raised on failed PerpleDb.insert's"""
+
+
+class RegistrationFailed(Exception):
+    """Exception raised on failed PerpleManager.registers's"""
+
+
+class PerpleManager(object):
+
+    def __init__(self, db):
+        self._db = db
+
+
+    def register(self, perple):
+        try:
+            self._db.insert(perple)
+        except BadInsert:
+            raise RegistrationFailed
+
+
+class PerpleDb(object):
+    """Ermah-person class!"""
+
+    def __init__(self, db):
+        self._db = tmp_database
+
+
+    def insert(self, perple):
+        try:
+            data = (perple.name, perple.age, )
+            insert_sql = 'INSERT INTO perple_table(name,age) VALUES (?,?);'
+
+            self._db.execute(insert_sql, data)
+            if self._db.rowcount == 0:
+                raise Exception('The insert failed')
+
+        except:
+            raise BadInsert
+
+
+class Perple(object):
+
+    age = None
+    name = None
+
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+
+
+class TestPerpleManager(object):
+
+
+    def setUp(self):
+        self.mox = mox.Mox()
+        self.dao = self.mox.CreateMock(PerpleDb)
+        self.manager = PerpleManager(self.dao)
+        self.perple = Perple('Kate', 42)
+
+
+    def test_register_successful_insert(self):
+        self.dao.insert(self.perple)
+        self.mox.ReplayAll()
+        self.manager.register(self.perple)
+        self.mox.VerifyAll()
+
+
+    def test_failed_insert(self):
+        self.dao.insert(self.perple).AndRaise(BadInsert("I don't like Kate!"))
+        self.mox.ReplayAll()
+        assert_raises(RegistrationFailed, self.manager.register, self.perple)
+        self.mox.VerifyAll()
+
+
