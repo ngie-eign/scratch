243eec7b361cb8c80970239a4902604a17955011
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-04-23 10:57:17 -0700

    Add a bunch of test scripts

---
+++ programming/python/profiler_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import time
+
+import cProfile
+import pstats
+import StringIO
+
+def loopy():
+    for i in range(20):
+        time.sleep(0.1)
+
+stats_keys = ['calls', 'cumulative']
+profiler = cProfile.Profile()
+profiler.enable()
+
+loopy()
+
+profiler.disable()
+stream = StringIO.StringIO()
+stats = pstats.Stats(profiler,
+                     stream=stream).sort_stats(*stats_keys)
+stats.print_stats()
+print('Profiling statistics: %s' % (stream.getvalue(), ))
