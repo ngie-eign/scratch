cf59d2d419e464e6bdc2e1e7fb5bce1638ca4179
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-04-24 14:41:10 -0700

    Commit some more hack/test scripts

---
+++ programming/sqlite_import_hack.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+db=$1
+csv=$2
+table=$3
+
+IFS="
+"
+while read line
+do
+	if [ -z "${header:-}" ]
+	then
+		header=$line
+	else
+		sqlite3 $db "INSERT INTO $table($header) VALUES($line)"
+	fi
+	shift
+done < $csv
