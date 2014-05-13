7d430a8d02ca11619d31eb7faa2916f87c12b72c
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-05-13 10:51:41 -0700

    PEP8 and python 2.7+ idioms

--- programming/python/csv_dict_reader_writer.py
+++ programming/python/csv_dict_reader_writer.py
@@ -5,6 +5,7 @@
 from collections import OrderedDict
 import cStringIO
 import csv
+import sys
 import traceback
 
 nfields = 10
@@ -14,6 +15,9 @@ d = OrderedDict([('f_' + str(i), str(i)) for i in xrange(nfields)])
 fieldnames = d.keys()
 spurious_fieldnames = fieldnames + ['extra']
 less_fieldnames = fieldnames[:-3]
+
+use_exec = True
+
 
 def print_csv(field_names, with_leftovers=False):
 
@@ -30,47 +34,61 @@ def print_csv(field_names, with_leftovers=False):
             dr_kwargs.update({
                 'restkey': restkey,
             })
-        print('dr_kwargs: %s' % dr_kwargs)
+        sys.stdout.write('dr_kwargs: %s\n' % (dr_kwargs, ))
         reader = csv.DictReader(f, **dr_kwargs)
-        for i, line in enumerate(reader, start=1):
-            print('Line %03d: %s (type=%s)' % (i, line, type(line), ))
+        for j, line in enumerate(reader, start=1):
+            sys.stdout.write('Line %03d: %s (type=%s)\n'
+                             % (j, line, type(line), ))
 
-            for field in fieldnames:
-                exec('%s = line.get(field)' % (field, ))
-
-            if False:
+            # The former idiom will work in all cases, but it causes pylint to
+            # complain about undefined variables referenced later on in the
+            # code.
+            #
+            # The latter idiom doesn't suffer from this caveat, but results in
+            # a tuple unpack error if the field names don't match the fields
+            # in a row.
+            #
+            # This is a programming error :)..
+            if use_exec:
+                for field in fieldnames:
+                    exec('%s = line.get(field)' % (field, ))
+            else:
                 (
-                f_0,
-                f_1,
-                f_2,
-                f_3,
-                f_4,
-                f_5,
-                f_6,
-                f_7,
-                f_8,
-                f_9,
+                    f_0,
+                    f_1,
+                    f_2,
+                    f_3,
+                    f_4,
+                    f_5,
+                    f_6,
+                    f_7,
+                    f_8,
+                    f_9,
                 ) = tuple([line.get(field) for field in field_names])
-            print('Values %03d: %s' % (i,
-                                       ', '.join(map(str,
-                                                     (f_0, f_1, f_2, f_3, f_4,
-                                                      f_5, f_6, f_7, f_8, f_9, )))))
+
+            sys.stdout.write('Values %03d: %s\n'
+                             % (j, ', '.join(map(str,
+                                                 (f_0, f_1, f_2, f_3, f_4,
+                                                  f_5, f_6, f_7, f_8, f_9,
+                                                  )))))
 
     finally:
         f.close()
 
-print('Exact number of fields')
+
+sys.stdout.write('Exact number of fields\n')
 print_csv(fieldnames)
 
-print('Spurious fields')
+sys.stdout.write('Spurious fields\n')
 print_csv(spurious_fieldnames)
 
-print('Less fields without leftovers')
+sys.stdout.write('Less fields without leftovers\n')
 try:
     print_csv(less_fieldnames)
 except ValueError:
-    print('OK (raised a ValueError). The exception received was:\n%s'
-          % (traceback.format_exc(), ))
+    sys.stdout.write('OK (raised a ValueError). The exception '
+                     'received was:\n%s\n'
+                     % (traceback.format_exc(), ))
 
-print('Less fields with leftovers')
+sys.stdout.write('Less fields with leftovers\n')
 print_csv(less_fieldnames, with_leftovers=True)
