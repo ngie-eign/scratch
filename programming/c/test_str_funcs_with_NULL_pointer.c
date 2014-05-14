83782be664c42f54d5a0d9592121f1cda77fc20b
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-05-14 10:50:11 -0700

    Mute warnings

--- programming/c/test_str_funcs_with_NULL_pointer.c
+++ programming/c/test_str_funcs_with_NULL_pointer.c
@@ -14,12 +14,14 @@ segfault_handler(int signo)
 int
 main(int argc, char **argv)
 {
+	char *null = NULL;
+
 	signal(SIGSEGV, segfault_handler);
 
 	if (argc > 1 && strcmp(argv[1], "strlen"))
-		printf("len: %d\n", strlen(NULL));
+		printf("len: %zu\n", strlen(null));
 	else
-		printf("cmp: %d\n", strcmp(NULL, ""));
+		printf("cmp: %d\n", strcmp(null, ""));
 
 	exit(0);
 }
