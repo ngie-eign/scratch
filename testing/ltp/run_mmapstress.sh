c3bd6bad9b07df78496517bd576a392fd64b7f8c
Garrett Cooper <yanegomi@gmail.com>
2014-07-10 15:29:56 -0700

    Improve test script
    1. Clamp down offsets to sysconf(_SC_PAGE_SIZE) amounts so the test
       apps no longer die when they get invalid offsets.
    2. Parallelize the runs
    3. Fix the filesystem high watermark code.
    4. Run tests at random instead of sequentially.
    
    Sponsored-by: EMC / Isilon Storage Division

--- testing/ltp/run_mmapstress.sh
+++ testing/ltp/run_mmapstress.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
+#
+# A script for running mmapstress from LTP on FreeBSD
+
+filesystem_high_watermark()
+{
+	echo $(( $(df -k . | awk 'NR > 1 { print $4 }') * 1024 * 8 / 10 ))
+}
 
 random_offset()
 {
-	local filesystem_high_watermark=$(( 9 * $(df -k . | awk 'NR > 1 { print $4 }') * 1024 / 10 / 8 ))
 
-	python2 -c "import random, sys
-sys.stdout.write(str(0.75 * random.randrange($filesystem_high_watermark)))
+	python2 -c "import os, random, sys
+sys.stdout.write(str(int(0.75 * random.randrange($(filesystem_high_watermark)) / os.sysconf('SC_PAGE_SIZE')) * os.sysconf('SC_PAGE_SIZE')))
 "
 }
 
 random_size()
 {
-	local filesystem_high_watermark=$(( $(df -k . | awk 'NR > 1 { print $4 }') * 1024 ))
 
 	python2 -c "import random, sys
-sys.stdout.write(str(random.randrange(1, $filesystem_high_watermark)))
+sys.stdout.write(str(int(0.75 * random.randrange($(filesystem_high_watermark)))))
 "
 }
 
@@ -25,9 +30,44 @@ sys.stdout.write(str(random.randrange(10)))
 "
 }
 
-for standalone in mmap-corruption01 mmapstress02 mmapstress03 mmapstress08; do
-	./$standalone
+STANDALONE_PROGS="mmap-corruption01 mmapstress02 mmapstress03 mmapstress08"
+PROGS="$STANDALONE_PROGS mmapstress01 mmapstress04"
+
+run_random_test()
+{
+	local _test=$(python2 -c "import random, sys
+sys.stdout.write(random.choice('$PROGS'.split()))
+")
+
+	case "$_test" in
+	mmapstress01)
+		./$_test -p $(sysctl -n kern.smp.cpus) -t $(random_time) -f $(random_size)
+		;;
+	mmapstress04)
+		./$_test $(mktemp -u) $(random_offset)
+		;;
+	*)
+		./$_test
+		;;
+	esac
+}
+
+spawn_tests()
+{
+	while :; do
+		run_random_test || break
+	done
+}
+
+for prog in $PROGS; do
+	if [ ! -x $prog ]; then
+		echo "${0##*/}: ERROR missing program: $prog"
+		exit 1
+	fi
 done
 
-./mmapstress01 -p $(sysctl -n kern.smp.cpus) -t $(random_time) -f $(random_size)
-./mmapstress04 $(mktemp -u) $(random_offset)
+: ${NUM_TEST_PROCS=$(sysctl -n kern.smp.cpus)}
+for cpu in $NUM_TEST_PROCS; do
+	spawn_tests &
+done
+wait
