a14aa512994f521cc7e6b6c9b6209d95d8a3ad3b
Garrett Cooper <yanegomi@gmail.com>
2014-07-10 17:49:41 -0700

    Uhm, how about we actually parallelize things?

--- testing/ltp/run_mmapstress.sh
+++ testing/ltp/run_mmapstress.sh
@@ -66,8 +66,10 @@ for prog in $PROGS; do
 	fi
 done
 
+i=0
 : ${NUM_TEST_PROCS=$(sysctl -n kern.smp.cpus)}
-for cpu in $NUM_TEST_PROCS; do
+while [ $i -lt $NUM_TEST_PROCS ]; do
 	spawn_tests &
+	: $(( i += 1 ))
 done
 wait
