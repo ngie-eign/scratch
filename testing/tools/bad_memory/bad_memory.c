d478ef57173a4beef67aec7780061d46fa7d2990
Garrett Cooper <yanegomi@gmail.com>
2014-07-22 01:57:07 -0700

    Fix build and linking issues with the module
    - Add missing headers.
    - Use sbufs for strings (silly me for always programming in userland)
    - Rename sysctl tree to test to make progress until I figure out the
      proper formula for sysctl(9) to populate properly without resulting
      in runtime linker errors
    - Finish off module definition
    - Fix some typos in function/macro calls
    
    Sponsored-by: EMC / Isilon Storage Division

--- testing/tools/bad_memory/bad_memory.c
+++ testing/tools/bad_memory/bad_memory.c
@@ -1,11 +1,12 @@
 #include <sys/param.h>
-#include <sys/module.h>
-#include <sys/systm.h>
 #include <sys/kernel.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/sbuf.h>
 #include <sys/sysctl.h>
+#include <sys/systm.h>
 
 MALLOC_DECLARE(M_BAD_MEMORY);
-
 MALLOC_DEFINE(M_BAD_MEMORY, "bad_memory", "Bad memory test malloc zone");
 
 static int
@@ -59,32 +60,59 @@ static struct callback_t {
 static int
 sysctl_test_bad_memory_operation(SYSCTL_HANDLER_ARGS)
 {
-	char *operation;
+	struct sbuf operation;
 	int error;
 	unsigned int i;
 
-	error = sysctl_handle_string(oidp, &operation, operation, req);
+	sbuf_new(&operation, NULL, 20, SBUF_FIXEDLEN);
+
+	error = sysctl_handle_string(oidp, sbuf_data(&operation),
+	    sbuf_len(&operation), req);
 	if (error)
 		return (error);
 
 	for (i = 0; i < nitems(callbacks); i++)
-		if (strcmp(callbacks[i].name, operation) == 0)
+		if (strcmp(callbacks[i].name, sbuf_data(&operation)) == 0)
 			callbacks[i].fn();
+
+	sbuf_delete(&operation);
 
 	return (EINVAL);
 }
 
-SYSCTL_DECLARE(_test); /* XXX: move this somewhere else */
-SYSCTL_DECLARE(_test_bad_memory);
+/* XXX: move these next few lines somewhere else */
+SYSCTL_DECL(_test);
+SYSCTL_ROOT_NODE(OID_AUTO, test, CTLFLAG_RW, 0, "Testing");
+
+/*
+ * XXX: needs some work because of unresolved symbols
+ *
+ * Should look at some of the drivers that employ hw.* to
+ * build their sysctl trees automagically
+ */
+#if 0
+SYSCTL_DECL(_test_bad_memory);
+static SYSCTL_NODE(_test_bad_memory, OID_AUTO, bad_memory, CTLFLAG_RD, 0,
+    "bad_memory testing sysctl node");
 
 SYSCTL_PROC(_test_bad_memory, OID_AUTO, operation, CTLTYPE_STRING|CTLFLAG_RW,
+    0, 0, sysctl_test_bad_memory_operation, "A",
+    "Perform an operation with the module to force a failure state in the "
+    "kernel related to memory allocation/dereferencing; supported operations "
+    "are: 'double_free', 'out_of_bounds', and 'uninitialized'");
+#else
+SYSCTL_PROC(_test, OID_AUTO, bad_memory_operation, CTLTYPE_STRING|CTLFLAG_RW,
     NULL, 0, sysctl_test_bad_memory_operation, "A",
     "Perform an operation with the module to force a failure state in the "
-    "kernel; supported operations are: 'double_free', 'out_of_bounds', and "
-    "'uninitialized'");
+    "kernel related to memory allocation/dereferencing; supported operations "
+    "are: 'double_free', 'out_of_bounds', and 'uninitialized'");
+#endif
 
-static moduledata_t bad_memory = {
+static moduledata_t bad_memory_moddata = {
 	"bad_memory",
 	load_bad_memory,
 	NULL,
 };
+
+MODULE_VERSION(bad_memory, 1);
+DECLARE_MODULE(bad_memory, bad_memory_moddata, SI_SUB_EXEC, SI_ORDER_ANY);
