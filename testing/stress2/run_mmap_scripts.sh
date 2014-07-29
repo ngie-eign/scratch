da77c2bb496ad7811e03a9dd351dc72e2226daec
Garrett Cooper <yanegomi@gmail.com>
2014-07-29 10:45:18 -0700

    Add a wrapper script for running the mmap* scripts from stress2
    This script protects the wrapper script and its parent from the
    OOM to avoid instances where the script was killed while the tests
    were being run, thus halting the test run
    
    The descendants beyond mmap*.sh aren't protected by design
    
    Sponsored-by: EMC / Isilon Storage Division

---
+++ testing/stress2/run_mmap_scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "$PPID" -o $PPID -le 0 ]
+then
+	echo "${0##*/}: ERROR: $PPID either undefined or less than 0"
+	exit 1
+fi
+
+# Protect this script from the OOM and its parent
+if protect -p $PPID
+then
+	# Remove protection after we're done
+	trap "protect -c -p $PPID"
+else
+	echo "${0##*/}: WARNING: calling protect(8) failed; this script might be killed by the OOM later"
+fi
+
+for i in mmap*.sh; do
+	protect sh ./$i
+done
