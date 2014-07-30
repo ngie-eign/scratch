42139fc5da99a61605b09b5f6cfedb6deec88754
Garrett Cooper <yanegomi@gmail.com>
2014-07-30 16:04:39 -0700

    Work on testcases for the malloc driver
    It seems like some of this support can't be tested [easily] until
    we get RUMP in the mix, in part because of INVARIANTS support (oddly
    enough). Seems like a bug to me...
    
    Sponsored-by: EMC / Isilon Storage Division

--- testing/freebsd/memguard/tests/t_memguard_malloc.sh
+++ testing/freebsd/memguard/tests/t_memguard_malloc.sh
@@ -1,7 +1,13 @@
 # TODO: interrogate vm.memguard.num_alloc and vm.memguard.minsize_reject
 # TODO: test out multiple options with vm.memguard.options
 
-malloc_size=16384
+original_desc=$(sysctl -n vm.memguard.desc)
+original_minsize=$(sysctl -n vm.memguard.minsize)
+original_options=$(sysctl -n vm.memguard.options)
+
+test_driver="memguard_malloc_helper"
+test_sysctl_prefix="test.$test_driver"
+malloc_size=8192
 
 # inuse memuse highuse requests
 #
@@ -21,45 +27,144 @@ END { exit(not_found) }
 '
 }
 
-atf_test_case test_MG_GUARD_AROUND
-test_MG_GUARD_AROUND_head()
+skip_if_invariants_enabled()
+{
+	if [ "$(sysctl -n kern.features.invariant_support)" = "1" ]; then
+		atf_skip "results in [non-immediate] INVARIANTS panic"
+	fi
+}
+
+set_test_sysctl()
+{
+	atf_check -o not-empty sysctl ${test_sysctl_prefix}_$1=$2
+}
+
+set_memguard_sysctl()
+{
+	atf_check -o not-empty sysctl vm.memguard.$1="$2"
+}
+
+reset_memguard_sysctls()
+{
+	set_memguard_sysctl desc "$original_desc"
+	set_memguard_sysctl minsize $original_minsize
+	set_memguard_sysctl options $original_options
+}
+
+set_memguard_desc()
+{
+	set_memguard_sysctl desc $test_driver
+}
+
+set_memguard_options()
+{
+	set_memguard_sysctl options $1
+}
+
+load_test_driver()
+{
+	kldstat -m $test_driver || atf_check kldload $test_driver
+}
+
+unload_test_driver()
+{
+	kldunload $test_driver
+}
+
+atf_test_case test_MG_GUARD_AROUND_negative
+test_MG_GUARD_AROUND_negative_head()
 {
 	:
 }
 
-test_MG_GUARD_AROUND_body()
+test_MG_GUARD_AROUND_negative_body()
 {
-	atf_skip "TODO: write this testcase"
+	skip_if_invariants_enabled
 
-	atf_check -o  vmstat -m
+	set_memguard_desc
+	set_memguard_options 1
+
+	load_test_driver
+
+	set_test_sysctl slab_size 1
+	set_test_sysctl slab_offset -1
+	set_test_sysctl allocate 1
 }
 
-atf_test_case test_MG_GUARD_ALLLARGE
-test_MG_GUARD_ALLLARGE_head()
+test_MG_GUARD_AROUND_negative_cleanup()
+{
+	reset_memguard_sysctls
+	unload_test_driver
+}
+
+atf_test_case test_MG_GUARD_AROUND_positive
+test_MG_GUARD_AROUND_positive_head()
 {
 	:
 }
 
-test_MG_GUARD_ALLLARGE_body()
+test_MG_GUARD_AROUND_positive_body()
 {
+	skip_if_invariants_enabled
 
-	atf_skip "TODO: write this testcase"
+	set_memguard_desc
+	set_memguard_options 1
 
-	atf_check -o match vmstat -m
+	load_test_driver
+
+	set_test_sysctl slab_size 1
+	set_test_sysctl slab_offset 2
+	set_test_sysctl allocate 1
+	reset_memguard_desc
 }
 
-atf_test_case test_MG_GUARD_NOFREE
-test_MG_GUARD_NOFREE_head()
+test_MG_GUARD_AROUND_positive_cleanup()
+{
+	reset_memguard_sysctls
+	unload_test_driver
+}
+
+atf_test_case test_MG_GUARD_AROUND_and_MG_GUARD_ALLLARGE
+test_MG_GUARD_AROUND_and_MG_GUARD_ALLLARGE_head()
 {
 	:
 }
 
-test_MG_GUARD_NOFREE_body()
+test_MG_GUARD_AROUND_and_MG_GUARD_ALLLARGE_body()
 {
-	atf_skip "TODO: write this testcase"
+	skip_if_invariants_enabled
 
-	vmstat -m
+	set_memguard_desc
+	set_memguard_options 3
+
+	load_test_driver
+
+	set_test_sysctl slab_size $malloc_size
+	set_test_sysctl slab_offset $(( $malloc_size + 1 ))
+	set_test_sysctl allocate 1
 }
+
+test_MG_GUARD_AROUND_and_MG_GUARD_ALLLARGE_cleanup()
+{
+	reset_memguard_sysctls
+	unload_test_driver
+}
+
+#
+# memguard_arena and memguard_physlimit are read-only tunables:
+#
+#306         /*
+#307          * When we pass our memory limit, reject sub-page allocations.
+#308          * Page-size and larger allocations will use the same amount
+#309          * of physical memory whether we allocate or hand off to
+#310          * uma_large_alloc(), so keep those.
+#311          */
+#312         if (vmem_size(memguard_arena, VMEM_ALLOC) >= memguard_physlimit &&
+#313             req_size < PAGE_SIZE) {
+#314                 addr = (vm_offset_t)NULL;
+#315                 memguard_fail_pgs++;
+#316                 goto out;
+#317         }
 
 atf_test_case test_fail_kva
 test_fail_kva_head()
@@ -70,6 +175,11 @@ test_fail_kva_head()
 test_fail_kva_body()
 {
 	atf_skip "TODO: write this testcase"
+
+	set_memguard_desc
+	set_memguard_options 3
+
+	load_test_driver
 
 	sysctl vm.memguard.fail_kva
 }
@@ -85,6 +195,11 @@ test_fail_pgs_body()
 
 	atf_skip "TODO: write this testcase"
 
+	set_memguard_desc
+	set_memguard_options 3
+
+	load_test_driver
+
 	sysctl vm.memguard.fail_pgs
 }
 
@@ -96,18 +211,39 @@ test_minsize_head()
 
 test_minsize_body()
 {
+	skip_if_invariants_enabled
 
-	atf_skip "TODO: write this testcase"
+	set_memguard_desc
+	set_memguard_options 3
 
-	sysctl vm.memguard.minsize
+	old_minsize_reject=$(sysctl -n vm.memguard.minsize_reject)
+
+	load_test_driver
+
+	set_memguard_sysctl minsize 2
+
+	set_test_sysctl slab_size 1
+	set_test_sysctl allocate 1
+
+	minsize_reject=$(sysctl -n vm.memguard.minsize_reject)
+
+	if [ $(( $old_minsize_reject + 1 )) -ne $minsize_reject ]; then
+		atf_fail "$old_minsize_reject + 1 != $minsize_reject"
+	fi
+}
+
+test_minsize_cleanup()
+{
+	reset_memguard_sysctls
+	unload_test_driver
 }
 
 atf_init_test_cases()
 {
-	atf_add_test_case test_MG_GUARD_AROUND
-	atf_add_test_case test_MG_GUARD_ALLLARGE
-	atf_add_test_case test_MG_GUARD_NOFREE
-	atf_add_test_case test_fail_kva
-	atf_add_test_case test_fail_pgs
+	atf_add_test_case test_MG_GUARD_AROUND_negative
+	atf_add_test_case test_MG_GUARD_AROUND_positive
+	atf_add_test_case test_MG_GUARD_AROUND_and_MG_GUARD_ALLLARGE
+	#atf_add_test_case test_fail_kva
+	#atf_add_test_case test_fail_pgs
 	atf_add_test_case test_minsize
 }
