6433be1a30484ca3bda320b2a615d1a67f92fac1
Garrett Cooper <yanegomi@gmail.com>
2014-08-21 14:44:54 -0700

    Fix typos and make script more extensible
    - Don't hardcode /dev/da1; allow user to provide a mix of devices and
      paths via $*
    - Allow bs= to be random
    - Provide random size= value if user specified a path (this was required
      for later versions of fio)
    - Remove randwrite,rw (not supported on later versions of fio)
    - Check for fio and print out a message if it can't be found
    - cat out the jobfile before calling fio for debugging purposes.
    - Allow ioengine to be a random ioengine that is supported by
      FreeBSD
    
    Sponsored by: EMC / Isilon Storage Division

--- testing/freebsd/tools/run_fio.sh
+++ testing/freebsd/tools/run_fio.sh
@@ -1,22 +1,77 @@
 #!/bin/sh
+#
+# usage: run_fio.sh device|file ...
 
 : ${PYTHON=python}
+
+random_bs()
+{
+	$PYTHON -c 'import random, sys;
+sys.stdout.write(str(random.randrange(512, 10241)))'
+}
+
+random_io_engine()
+{
+	$PYTHON -c 'import random, sys;
+rw_choices = "mmap posixaio psync pvsync sync vsync".split();
+sys.stdout.write(random.choice(rw_choices))'
+}
 
 random_io_operation()
 {
 	$PYTHON -c 'import random, sys;
-rw_choices = "read write randwrite randread rw,readwrite randrw".split()
-sys.stdout.write(random.choice(rw.choices))'
+rw_choices = "read write randwrite randread randrw".split();
+sys.stdout.write(random.choice(rw_choices))'
 }
 
-JOBS=$(sysctl -n kern.smp.cpus)
+random_size()
+{
+	local size
+
+	$need_size || return
+
+	size=$($PYTHON -c 'import random, sys;
+sys.stdout.write(str(random.randrange(1, 1024 * 1024)))')
+	echo "size=$size"
+}
+
+if ! command -v fio; then
+	echo "${0##*/}: ERROR: please install fio"
+	exit 1
+fi
+
+need_size=false
+for path in $*; do
+	if [ -c $path ]; then
+		:
+	else
+		if [ ! -e $path ] && ! : > $path; then
+			echo "${0##*/}: ERROR: could not create path: $path"
+			exit 1
+		fi
+		need_size=true
+	fi
+	PATHS="${PATHS:+$PATHS:}$path"
+done
+
+echo $PATHS; 
+: ${NUM_JOBS=$(sysctl -n kern.smp.cpus)}
+
+JOBFILE=fio.job
 while :; do
-	cat > fio.job <<EOF
+	cat > $JOBFILE <<EOF
 [jobs]
-filename=/dev/da1
+filename=$PATHS
+ioengine=$(random_io_engine)
 rw=$(random_io_operation)
 numjobs=$NUM_JOBS
-bs=10m
+bs=$(random_bs)
+$(random_size)
 EOF
-	fio fio.job
+
+	echo ">>>> START $JOBFILE <<<<"
+	cat $JOBFILE
+	echo ">>>> END $JOBFILE <<<<"
+
+	fio $JOBFILE
 done
