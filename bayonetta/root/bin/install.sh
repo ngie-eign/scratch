47f42b695a0aa6a0ed6bf6e6f068d9223dfe858f
Ngie Cooper <garrett.cooper@isilon.com>
2016-11-21 11:33:36 -0800

    Don't run mergemaster with sudo
    This only delayed issues where my running the command as !root would
    fail when running make installkernel

--- bayonetta/root/bin/install.sh
+++ bayonetta/root/bin/install.sh
@@ -5,7 +5,7 @@ set -e
 : ${DESTDIR=/}
 export DESTDIR
 
-sudo mergemaster -iU -p -m $PWD -D $DESTDIR
+mergemaster -iU -p -m $PWD -D $DESTDIR
 $(dirname $0)/installkernel.sh || exit
 make installworld
-sudo mergemaster -iU -m $PWD -D $DESTDIR
+mergemaster -iU -m $PWD -D $DESTDIR
