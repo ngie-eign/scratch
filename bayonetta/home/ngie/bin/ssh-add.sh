a21fd37f12195570db04624591b84a976c3d7d68
Ngie Cooper <yaneurabeya@gmail.com>
2017-04-07 12:33:40 -0700

    Updates/improvements
    - Cache the ssh-agent info so one can merely source the environment and
      use the running ssh-agent instance.
    - Factor out `id_rsa_FreeBSD` so it isn't explicitly added when the
      script is run, but instead conditionally added if present. This will
      allow me to use this script on clients that lack my FreeBSD SSH key.

--- bayonetta/home/ngie/bin/ssh-add.sh
+++ bayonetta/home/ngie/bin/ssh-add.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-set +ae
-eval `ssh-agent`
+SSH_AGENT_ENV=/tmp/ssh-agent-env.$(whoami)
+if [ -f $SSH_AGENT_ENV ]; then
+	(
+	. $SSH_AGENT_ENV >/dev/null
+	pgrep ssh-agent | grep -q $SSH_AGENT_PID &&
+	    kill -0 $SSH_AGENT_PID
+	if [ $? -ne 0 ]; then # Not running; nuke stale environment script
+		rm -f $SSH_AGENT_ENV
+		exit 1
+	fi
+	exit 0
+	)
+fi
+if [ ! -f $SSH_AGENT_ENV ]; then
+	if ! ssh-agent > $SSH_AGENT_ENV; then
+		rm -f $SSH_AGENT_ENV
+		exit 1
+	fi
+fi
+. $SSH_AGENT_ENV >/dev/null
 ssh-add
-ssh-add ~/.ssh/id_rsa_FreeBSD
+for extra_ssh_key in ~/.ssh/id_rsa_FreeBSD; do
+	if [ -e $extra_ssh_key ]; then
+		ssh-add $extra_ssh_key
+	fi
+done
