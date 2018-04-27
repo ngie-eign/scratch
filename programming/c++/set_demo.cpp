ee8fd4ad42d211c639864b8b43f2a496bd5485b1
Enji Cooper <ngie@fb.com>
2018-04-27 14:39:47 -0700

    Add a std::unordered_set demo app and a corresponding Makefile

---
+++ programming/c++/set_demo.cpp
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main(void)
+{
+  unordered_set<string> a_set({"foo"});
+
+  assert(a_set.size() == 1);
+  a_set.insert("bar");
+  assert(a_set.size() == 2);
+  a_set.insert({"rab", "oof"});
+  assert(a_set.size() == 4);
+  a_set.emplace("baz");
+  assert(a_set.size() == 5);
+
+  for (auto elem : a_set) {
+    cout << elem << endl;
+  }
+
+  int i = 0;
+  for (auto it = a_set.begin(); it != a_set.end(); i++, it++) {
+    cout << i << ": " << *it << endl;
+  }
+
+  return 0;
+}
