37efff57008e8a233aa9aa94cf8e99307a58e3f8
Enji Cooper <yaneurabeya@gmail.com>
2019-02-01 15:24:41 -0800

    Remove duplicate directory for picasso
    This will be based on profiles/base/freebsd instead.

--- picasso/home/ngie/bin/run-kyua-integrated-tap-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-[ -f Kyuafile ] || exit
-grep tap_test_program Kyuafile | sed -e 's,.*name=",,' -e 's,".*,,g' |
-    xargs -n 1 -I % prove -v ./%
--- picasso/root/bin/clean_stale_kernel_debug_symbols.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /usr/lib/debug/
-find boot/ -type d -maxdepth 1 -mindepth 1 | \
-while read d; do
-	[ -d /$d ] || rm -Rf $d
-done
