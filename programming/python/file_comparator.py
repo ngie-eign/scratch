6d16dd2d64799fdcf490a463f72b455ea210dd61
Enji Cooper <yaneurabeya@gmail.com>
2019-03-12 15:51:37 -0700

    Make the example work with py3

--- programming/python/file_comparator.py
+++ programming/python/file_comparator.py
@@ -7,22 +7,22 @@ import tempfile
 
 
 def compare_pattern(pattern, filename, offset):
-    with open(filename, 'r+b') as fd:
+    with open(filename, "r+b") as fd:
         fd.seek(offset)
-        return pattern == fd.read(len(pattern))
+        return pattern == fd.read(len(pattern)).decode("ascii")
 
 
 def write_pattern(pattern, filename, offset):
-    with open(filename, 'w+b') as fd:
+    with open(filename, "w+b") as fd:
         fd.seek(offset)
-        fd.write(pattern)
+        fd.write(pattern.encode("ascii"))
 
 
 # a-z,A-Z,0-9
 ALPHANUMERIC_PATTERN = \
-        ''.join([chr(i) for i in xrange(ord('a'), ord('z')+1)]) + \
-        ''.join([chr(i) for i in xrange(ord('A'), ord('Z')+1)]) + \
-        ''.join([chr(i) for i in xrange(ord('0'), ord('9')+1)])
+        "".join([chr(i) for i in range(ord("a"), ord("z")+1)]) + \
+        "".join([chr(i) for i in range(ord("A"), ord("Z")+1)]) + \
+        "".join([chr(i) for i in range(ord("0"), ord("9")+1)])
 
 
 def run(filename, pattern, offset=0):
@@ -33,7 +33,7 @@ def run(filename, pattern, offset=0):
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--offset', default=0, type=int)
+    parser.add_argument("--offset", default=0, type=int)
 
     args = parser.parse_args()
 
@@ -43,10 +43,11 @@ def main(argv=None):
         run(filename, pattern, offset=args.offset)
     finally:
         if os.path.exists(filename):
-            sys.stdout.write(open(filename, 'r+b').read() + '\n')
-            print(os.stat(filename))
+            with open(filename, "r+b") as fd:
+                print(fd.read().decode("ascii"))
+            print("\n%r" % (repr(os.stat(filename))))
             os.unlink(filename)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(argv=sys.argv)
