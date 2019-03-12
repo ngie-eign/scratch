7ddf2bbefb9cb18fe312400c988f26c9c0030688
Enji Cooper <yaneurabeya@gmail.com>
2019-03-12 13:41:16 -0700

    Make the script work with py3

--- programming/python/print_conjunction.py
+++ programming/python/print_conjunction.py
@@ -7,7 +7,7 @@ comma ;)..
 import types
 
 def join(l, conjunction='and', oxford_comma=True):
-    if type(l) not in (types.ListType, types.TupleType):
+    if type(l) not in (list, tuple):
         raise TypeError('l is not a list')
     if len(l) <= 1:
         return ''.join(l)
@@ -17,13 +17,13 @@ def join(l, conjunction='and', oxford_comma=True):
                            l[-1])
 
 
-print join(list('ab'))
-print join(list('abcd'))
-print join(['a'])
-print join([])
+print(join(list('ab')))
+print(join(list('abcd')))
+print(join(['a']))
+print(join([]))
 
-print join(list('abcd'), oxford_comma=False)
-print join(['a'], oxford_comma=False)
+print(join(list('abcd'), oxford_comma=False))
+print(join(['a'], oxford_comma=False))
 
-print join(list('abcd'), conjunction='or')
-print join(['a'], conjunction='or')
+print(join(list('abcd'), conjunction='or'))
+print(join(['a'], conjunction='or'))
