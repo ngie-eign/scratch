820cac7559424b4eeee6b46dadb6b2696b46c07d
Enji Cooper <yaneurabeya@gmail.com>
2019-07-30 16:36:30 -0700

    black-ify the test

--- programming/python/inspect_test.py
+++ programming/python/inspect_test.py
@@ -1,15 +1,18 @@
 import inspect
 
+
 def foo():
     frame = inspect.stack()[1][0]
     name = frame.f_code.co_name
-    print('current frame: %r (type = %s)' % (dir(frame.f_code), type(frame.f_code), ))
-    print('name = %s' % (name, ))
+    print("current frame: %r (type = %s)" % (dir(frame.f_code), type(frame.f_code)))
+    print("name = %s" % (name,))
     module = inspect.getmodule(name)
-    print('module = %s' % (module, ))
+    print("module = %s" % (module,))
+
 
 def bar():
     foo()
+
 
 bar()
 
