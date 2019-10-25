24af55103bc781dbb30eef5d6e0e3aa506d1950a
Enji Cooper <yaneurabeya@gmail.com>
2019-10-25 15:22:18 -0700

    Clean up merge-git-branches.sh
    A few things I noted about recent changes where I'm basically trying
    to merge against multiple remotes with a single clone is the need to
    map many things to one remote but not others, and the need to not
    merge everything to a single supported remote.
    
    Example:
    
    Say branch A was on remote X and branch B was on remote Y. Previously
    it would mirror all checked out branches on remote X to remote X and
    vice versa, which is far from desirable--hence the need to run
    `git push -f X Y`, in a 1:1 manner, for each appropriate remote.
    
    I [still] think this approach is clumsy and while it does better,
    this script is approaching the point of needing to be integrated
    into better tools/frameworks that integrate with git to get a greater
    degree of introspection, instead of having to define a funky naming
    scheme where topics could be mapped back to a single master branch.

--- profiles/base/unix/home/ngie/bin/merge-git-branches.sh
+++ profiles/base/unix/home/ngie/bin/merge-git-branches.sh
@@ -1,39 +1,78 @@
 #!/bin/sh
 
+DEBUG=false
+debug()
+{
+	if ! $DEBUG; then
+		# To appease `set -e`, return early in the negative path with
+		# `0`.
+		return 0
+	fi
+	echo "${0##*/}: DEBUG: $*"
+}
+
+error()
+{
+	echo "${0##*/}: ERROR: $*"
+	exit 1
+}
+
 set -e
 # Avoid the glob ;).
 original_branch=$(git branch -l | awk '$1 == "*" { print $NF }')
 trap "git checkout $original_branch" EXIT
 cd "$(git rev-parse --show-toplevel)"
+: ${DO_NOT_MIRROR_UPSTREAM_IN_CLONE=false}
 : ${GIT_MASTER=master}
 : ${GIT_UPSTREAM=upstream}
-git checkout $GIT_MASTER
-git pull --all
-git merge $GIT_UPSTREAM/$GIT_MASTER
+if $DO_NOT_MIRROR_UPSTREAM_IN_CLONE; then
+	debug "Not mirroring $GIT_UPSTREAM/$GIT_MASTER in clone. Fetching all repos instead."
+	git fetch --all
+else
+	git checkout $GIT_MASTER
+	git pull --all
+	git merge $GIT_UPSTREAM/$GIT_MASTER
+fi
 branches=$(git branch -l | awk '$1 != "*" { print $NF }' | sort -du || :)
-for branch in $branches
-do
+# If the end-user asked for a particular expression to whitelist
+if [ -n "$WHITELIST_FILTER" ]; then
+	debug "Before whitelist: $branches"
+	branches=$(echo "$branches" | egrep "$WHITELIST_FILTER" || :)
+	debug "After whitelist: $branches"
+fi
+for branch in $branches; do
+	# Skip over branches that need to be merged manually.
+	# TODO: determine parent in a more intelligent manner.
+	if [ -n "$BLACKLIST_FILTER" ] && echo "$branch" | egrep -q "$BLACKLIST_FILTER"; then
+		debug "$branch matched blacklist"
+		continue
+	fi
 	git checkout $branch
 	# Pull in the latest committed changes so they don't get stomped on by
 	# another force-pushed set of changes.
-	tracking_branch_repo=`git config branch.$branch.remote`
-	git rebase $tracking_branch_repo/$branch
-	case "$branch" in
-	*/*)
-		parent_branch=$GIT_UPSTREAM/$branch
-		;;
-	*)
-		parent_branch=$GIT_MASTER
-		;;
-	esac
+	if ! branch_remote=$(git config branch.$branch.remote); then
+		error "Could not determine remote for $branch"
+	fi
+	git rebase $branch_remote/$branch
 	git merge --no-edit $branch
+	if parent_branch=$(git config branch.$branch.parent); then
+		debug "Using manual override for $branch (child) to $parent_branch (parent)"
+	else
+		case "$branch" in
+		*/*)
+			parent_branch=$GIT_UPSTREAM/$branch
+			;;
+		*)
+			parent_branch=$GIT_MASTER
+			;;
+		esac
+		debug "Guessing $branch's parent is $parent_branch"
+	fi
 	git rebase $parent_branch
+	if ${AUTO_PUSH:-true}; then
+		git push -f $branch_remote $branch
+	fi
 done
-if ${AUTO_PUSH:-true}
-then
-	git push --all -f
-fi
-if ${AUTO_GC:-true}
-then
+if ${AUTO_GC:-true}; then
 	git gc
 fi
