e725ccaf7909abff89e0bea80e4801b9bf2f3355
Enji Cooper <yaneurabeya@gmail.com>
2020-05-03 19:04:57 -0700

    Run code through black

--- programming/python/service/service.py
+++ programming/python/service/service.py
@@ -11,15 +11,13 @@ import os
 import sys
 
 import daemon
+
 # pylint: disable=F0401
 import lockfile.pidlockfile as pidlockfile
 
-from .logger import (
-     get_logger,
-     init_logger,
-)
+from .logger import get_logger, init_logger
 
-VAR_RUN_PATH = '/var/run'
+VAR_RUN_PATH = "/var/run"
 
 
 class Service(object):
@@ -38,7 +36,6 @@ class Service(object):
     >>> ServiceClass('my_service').main()
     """
 
-
     def __init__(self, service_name):
         """
         :param service_name: a unique, easy to search for service name, e.g.
@@ -49,7 +46,6 @@ class Service(object):
         self.logger = None
         self.pidfile = None
         self.service_name = service_name
-
 
     def parse_args_pre(self, parser):
         """Operations to perform on `parser` before parsing the arguments.
@@ -62,7 +58,6 @@ class Service(object):
 
         self.add_extra_arguments(parser)
 
-
     def parse_args_post(self, args):
         """Operations to perform on `args` after parsing the arguments.
 
@@ -74,12 +69,10 @@ class Service(object):
 
         self.parse_extra_arguments(args)
 
-
     def run(self):
         """Abstract method that defines the core behavior of the service.
         """
         raise NotImplementedError
-
 
     def setup(self, argv=None, usage=None):
         """Performs of the "setup" operations, i.e. parsing arguments, reading
@@ -92,9 +85,9 @@ class Service(object):
 
         parser = ArgumentParser(usage=usage)
 
-        parser.add_argument('--conf', '-C', dest='config_file',
-                            help='configuration file',
-                            )
+        parser.add_argument(
+            "--conf", "-C", dest="config_file", help="configuration file"
+        )
 
         self.parse_args_pre(parser)
         args = parser.parse_args(argv)
@@ -105,25 +98,22 @@ class Service(object):
 
         self.config = RawConfigParser()
         if not self.config.read(args.config_file):
-            sys.exit('Could not read config file: %s' % (args.config_file, ))
-
+            sys.exit("Could not read config file: %s" % (args.config_file,))
 
     def main(self, argv=None, usage=None):
         """main wrapper function"""
 
         self.setup(argv=argv, usage=usage)
 
-        self.logger.info('Starting service')
+        self.logger.info("Starting service")
         try:
             self.run()
         except (KeyboardInterrupt, SystemExit):
             pass
-        self.logger.info('Stopping service')
-
+        self.logger.info("Stopping service")
 
     def add_extra_arguments(self, parser):
         """Add additional service arguments"""
-
 
     def parse_extra_arguments(self, args):
         """Parse additional service arguments"""
@@ -145,28 +135,28 @@ class Daemon(Service):
 
         See also: Service.parse_args_pre(..)
         """
-        default_pidfile_path = \
-            os.path.join(VAR_RUN_PATH, '%s.pid' % (self.service_name, ))
+        default_pidfile_path = os.path.join(
+            VAR_RUN_PATH, "%s.pid" % (self.service_name,)
+        )
 
-        parser.add_argument('--no-daemon', '-D', dest='daemonize',
-                            action='store_false',
-                            default=True,
-                            help='do not daemonize',
-                            )
-        parser.add_argument('--pidfile', '-p', dest='pidfile',
-                            help='PID file',
-                            default=default_pidfile_path,
-                            )
-        parser.add_argument('--runas-group',
-                            help='Group to run as',
-                            default='zuser',
-                            )
-        parser.add_argument('--runas-user',
-                            help='User to run as',
-                            default='zuser',
-                            )
+        parser.add_argument(
+            "--no-daemon",
+            "-D",
+            dest="daemonize",
+            action="store_false",
+            default=True,
+            help="do not daemonize",
+        )
+        parser.add_argument(
+            "--pidfile",
+            "-p",
+            dest="pidfile",
+            help="PID file",
+            default=default_pidfile_path,
+        )
+        parser.add_argument("--runas-group", help="Group to run as", default="zuser")
+        parser.add_argument("--runas-user", help="User to run as", default="zuser")
         super(Daemon, self).parse_args_pre(parser)
-
 
     def parse_args_post(self, args):
         """Handle additional argument parsing via `args` for Daemons.
@@ -183,7 +173,6 @@ class Daemon(Service):
 
         super(Daemon, self).parse_args_post(args)
 
-
     def main(self, argv=None, usage=None):
         """Handle either running in the foreground or creating a
         pidfile/daemonizing
@@ -193,33 +182,32 @@ class Daemon(Service):
 
         self.setup(argv=argv, usage=None)
 
-
         if not self.daemonize:
-            self.logger.info('Starting daemon in foreground')
+            self.logger.info("Starting daemon in foreground")
             try:
                 self.run()
             except (KeyboardInterrupt, SystemExit):
                 pass
-            self.logger.info('Stopping daemon in foreground')
+            self.logger.info("Stopping daemon in foreground")
             return
 
-        self.logger.info('Starting daemon')
+        self.logger.info("Starting daemon")
         try:
             pidlock_obj = pidlockfile.PIDLockFile(self.pidfile, timeout=0)
         except Exception:
-            self.logger.exception('Failed to create the pidfile object')
+            self.logger.exception("Failed to create the pidfile object")
             raise
 
         try:
-            with daemon.DaemonContext(pidfile=pidlock_obj,
-                                      gid=self.runas_group,
-                                      uid=self.runas_user):
-                self.logger.info('Entering run')
+            with daemon.DaemonContext(
+                pidfile=pidlock_obj, gid=self.runas_group, uid=self.runas_user
+            ):
+                self.logger.info("Entering run")
                 self.run()
         except (KeyboardInterrupt, SystemExit):
             pass
         except Exception:
-            self.logger.exception('Failed to daemonize')
+            self.logger.exception("Failed to daemonize")
             raise
 
-        self.logger.info('Stopping daemon')
+        self.logger.info("Stopping daemon")
