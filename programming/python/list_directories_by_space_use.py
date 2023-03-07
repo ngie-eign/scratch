fb5d66f8ea7a4052778cc514560778636d12f649
Enji Cooper <yaneurabeya@gmail.com>
2023-03-07 01:04:56 -0800

    Uplift to python 3.9-compatible idioms
    - Add type hints.
    - Use pathlib, et, al.
    - Paint black.

--- programming/python/list_directories_by_space_use.py
+++ programming/python/list_directories_by_space_use.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
-
-from __future__ import division
-from __future__ import print_function
+#!/usr/bin/env python3
 
 import argparse
 import functools
 import multiprocessing  # Oh, how I long for you, `await`...
 import os
+import pathlib
 import pwd
 import subprocess
 import sys
-try:
-    from typing import List, Optional  # noqa: F401
-except ImportError:
-    pass
+from os import PathLike
+from typing import Any
+from typing import Optional
+from typing import Union
 
 from humanize.filesize import naturalsize
 
@@ -21,53 +19,55 @@ from humanize.filesize import naturalsize
 DEFAULT_CUTOFF = 10
 DEFAULT_THRESHOLD = 90
 
+PathIsh = Union[PathLike, str]
+
 
 @functools.total_ordering
-class Path:
-
-    def __init__(self, path, size):
-        # type: (str, int) -> None
+class ComparablePath:
+    def __init__(self, path: PathIsh, size: int):
         if size < 0:
-            raise ValueError("size < 0: %d" % (size))
+            raise ValueError(f"size < 0: {size:d}")
 
         self.path = path
         self.size = size
 
-    def __eq__(self, other):
-        # type: (Path) -> bool
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, ComparablePath):
+            return NotImplemented
         return self.size == other.size
 
-    def __lt__(self, other):
-        # type: (Path) -> bool
+    def __lt__(self, other: Any) -> bool:
+        if not isinstance(other, ComparablePath):
+            return NotImplemented
         return self.size < other.size
 
-    def __repr__(self):
-        # type: () -> str
-        return "%s(%r, %r)" % (self.__class__.__name__, self.path, self.size)
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.path!r}, {self.size})"
 
 
-def get_path_info(directory):
-    # type: (str) -> Path
-    """Get the size of a directory
+def get_path_info(directory: PathIsh) -> ComparablePath:
+    """Get the size of a directory.
 
     :param directory: the directory to get the size for.
-    :return: a Path object for `directory`.
+    :return: a ComparablePath object for `directory`.
     """
 
-    """
-    e.g.,
+    #
+    # e.g.,
+    #
+    # % sudo du -sB 1 /local/home/ecooper
+    # 4       /local/home/ecooper
 
-    % sudo du -bs /local/home/ecooper
-    4       /local/home/ecooper
-    """
-    output = subprocess.check_output(["sudo", "du", "-bs", directory]).strip()
-    size, path = output.split()
+    output = subprocess.check_output(
+        ["sudo", "du", "-sB", "1", str(directory)],
+        text=True,
+    )
+    size, path = output.strip().split(maxsplit=2)
 
-    return Path(path, int(size))
+    return ComparablePath(path, int(size))
 
 
-def should_gather_results(directory, min_threshold):
-    # type: (str, int) -> bool
+def should_gather_results(directory: PathIsh, min_threshold: int) -> bool:
     """Should results be gathered for `directory` based on `min_threshold`?
 
     :param directory: directory to determine whether or not the results should
@@ -77,18 +77,14 @@ def should_gather_results(directory, min_threshold):
     :return: True if the results should be gathered; False otherwise.
     """
 
-    """
-    e.g.,
-    $ df -P /local/home
-    Filesystem     1024-blocks       Used Available Capacity Mounted on
-    /dev/sdb1       2113645484 2006254976         0     100% /local/home
-    """
-    output = subprocess.check_output(
-        ["df", "-P", directory],
-    )
+    # e.g.,
+    # $ df -P /local/home
+    # Filesystem     1024-blocks       Used Available Capacity Mounted on
+    # /dev/sdb1       2113645484 2006254976         0     100% /local/home
+    output = subprocess.check_output(["df", "-P", directory], text=True)
 
     lines = output.splitlines()
-    assert len(lines) == 2, "Output unexpected: %s" % output
+    assert len(lines) == 2, f"Output unexpected: {output}"
     fields = lines[-1].split()[1:3]
     capacity, used = [int(field) for field in fields]
 
@@ -97,41 +93,28 @@ def should_gather_results(directory, min_threshold):
     return min_threshold < 100 * used / capacity
 
 
-def gather_path_info_for_root(root):
-    # type: (str) -> List(Path)
-    """
+def gather_path_info_for_root(root: pathlib.Path) -> list[ComparablePath]:
+    subdirs = list(root.iterdir())
 
-    """
-
-    subdirs = [
-        os.path.join(root, subdir)
-        for subdir in os.listdir(root) if subdir not in (".", "..")
-    ]
-
-    pool = multiprocessing.Pool()
-    try:
+    with multiprocessing.Pool() as pool:
         results = pool.map_async(get_path_info, subdirs)
-        path_info_objs = [path_obj for path_obj in results.get()]
-        pool.close()
-    except:
-        pool.terminate()
-        raise
-    finally:
-        pool.join()
-
+        path_info_objs = list(results.get())
     return path_info_objs
 
 
-def main(argv=None):
-    # type: Optional[List[str]] -> None
-    """main"""
+def main(argv: Optional[list[str]] = None) -> int:
+    """Eponymous main function."""
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--cutoff", default=DEFAULT_CUTOFF, type=int,
+        "--cutoff",
+        default=DEFAULT_CUTOFF,
+        type=int,
     )
     parser.add_argument(
-        "--threshold", default=DEFAULT_THRESHOLD, type=int,
+        "--threshold",
+        default=DEFAULT_THRESHOLD,
+        type=int,
     )
     parser.add_argument(
         "rootdir",
@@ -148,27 +131,27 @@ def main(argv=None):
     #    sort them by size.
 
     if not should_gather_results(args.rootdir, args.threshold):
-        sys.exit(0)
+        return 0
 
-    homedirs_with_sizes = gather_path_info_for_root(args.rootdir)
+    homedirs_with_sizes = gather_path_info_for_root(pathlib.Path(args.rootdir))
     homedirs_with_sizes_sorted = sorted(homedirs_with_sizes, reverse=True)
 
     # print(homedirs_with_sizes_sorted)
     max_elems = min(args.cutoff, len(homedirs_with_sizes))
     for i, path_obj in enumerate(homedirs_with_sizes_sorted[:max_elems], 1):
-        owner = os.stat(path_obj.path).st_uid
+        owner_uid = os.stat(path_obj.path).st_uid
         try:
-            pwent = pwd.getpwuid(owner)
+            pwent = pwd.getpwuid(owner_uid)
         except KeyError:
-            pass
+            owner = str(owner_uid)
         else:
             owner = pwent.pw_name
 
         subdir_size_humanized = naturalsize(path_obj.size, binary=True)
-        print("%2d. %80s %40s %s" % (
-            i, path_obj.path, owner, subdir_size_humanized
-        ))
+        print(f"{i:2d}. {path_obj.path:80s} {owner:40s} {subdir_size_humanized}")
+
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
