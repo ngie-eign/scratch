e8b9ec70da101c1dcd1c4e3767688a446db09b85
Enji Cooper <yaneurabeya@gmail.com>
2023-03-07 01:08:53 -0800

    Add a simple script for creating Makefile.crate files
    If only I had written this earlier... it would have saved me 0.5-1 hour
    of wasted time.

---
+++ programming/python/cargo_lock_to_makefile_cargo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Helper script for creating Makefile.crates for FreeBSD ports.
+"""
+
+import argparse
+import sys
+from typing import Optional
+
+try:
+    import tomllib
+except ImportError:
+    import toml as tomllib
+
+
+def main(argv: Optional[list[str]] = None) -> int:
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("cargo_lock_file", type=argparse.FileType("r"))
+
+    args = argparser.parse_args(args=argv)
+
+    with args.cargo_lock_file as fp:
+        print(
+            "CARGO_CRATES=    {}".format(
+                " \\\n\t".join(
+                    p["name"] + "-" + p["version"] for p in toml.load(fp)["package"]
+                )
+            )
+        )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
