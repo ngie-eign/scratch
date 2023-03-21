8d4735453efb8faa9e8a81f4b6e164d1db7e08fb
Enji Cooper <yaneurabeya@gmail.com>
2023-03-20 18:51:37 -0700

    Uplift script to be compatible with python3
    This script now uses python3.9+ compatible idioms to function.

--- profiles/base/freebsd/scratch/scripts/python/sendmail.py
+++ profiles/base/freebsd/scratch/scripts/python/sendmail.py
@@ -14,31 +14,49 @@ echo 'hello world!' | \
 Enji Cooper, October 2013
 """
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.Utils import formatdate
+import argparse
 import getpass
+import logging
 import os
 import select
 import smtplib
 import socket
 import sys
 import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from typing import Optional
 
 
-def add_attachments(attachment_files):
+DEBUG = False
+
+
+def add_attachments(attachment_files: list[os.PathLike]) -> list[MIMEText]:
     attachments = []
     for attachment_file in attachment_files:
-        attachment = MIMEText(open(attachment_file, 'rb').read(),
-                              _charset='utf-8')
-        attachment.add_header('Content-Disposition', 'attachment',
-                              filename=os.path.basename(attachment_file))
+        with open(attachment_file, "rb") as attachment_fp:
+            attachment = MIMEText(attachment_fp.read(), _charset="utf-8")
+        attachment.add_header(
+            "Content-Disposition",
+            "attachment",
+            filename=os.path.basename(attachment_file),
+        )
         attachments.append(attachment)
     return attachments
 
 
-def do_email(mailserver, port, user, domain, password, recipients, message,
-             subject, attachments=None):
+def do_email(
+    mailserver: str,
+    port: int,
+    user: str,
+    domain: str,
+    password: str,
+    recipients: list[str],
+    message: str,
+    subject: str,
+    attachments: Optional[os.PathLike] = None,
+):
     smtp_args = []
     if mailserver:
         smtp_args.append(mailserver)
@@ -50,12 +68,13 @@ def do_email(mailserver, port, user, domain, password, recipients, message,
 
     server = smtplib.SMTP(*smtp_args, timeout=10)
     try:
-        #server.set_debuglevel(1)
+        if DEBUG:
+            server.set_debuglevel(1)
         server.ehlo()
-        if server.has_extn('STARTTLS'):
+        if server.has_extn("STARTTLS"):
             server.starttls()
 
-        sender = '@'.join([user, domain]).encode('utf-8')
+        sender = "@".join([user, domain]).encode("utf-8")
 
         if attachments:
             msg = MIMEMultipart()
@@ -63,62 +82,67 @@ def do_email(mailserver, port, user, domain, password, recipients, message,
             for attachment in attachments:
                 msg.attach(attachment)
         else:
-            msg = MIMEText(message, _charset='utf-8')
-        msg['From'] = sender
-        msg['To'] = ', '.join(recipients)
-        msg['Date'] = formatdate()
-        msg['Subject'] = subject
+            msg = MIMEText(message, _charset="utf-8")
+        msg["From"] = sender
+        msg["To"] = ", ".join(recipients)
+        msg["Date"] = formatdate()
+        msg["Subject"] = subject
         msg = msg.as_string()
 
         try:
             if password:
                 server.login(user, password)
-        except Exception as e:
-            print('Error logging into server: %r' % (repr(e)))
-            #time.sleep(10)
+        except Exception:
+            logging.exception("Error logging into server")
+            # time.sleep(10)
         res = server.sendmail(sender, recipients, msg)
-        #if res:
-        #    print('Error sending mail: %r' % (repr(res)))
-        #else:
-        #    print('Email sent!\n%s' % (msg, ))
+        if DEBUG:
+            if res:
+                print(f"Error sending mail: {res!r}")
+            else:
+                print(f"Email sent!\n{msg}")
     finally:
         server.quit()
 
 
 def main():
-    import optparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        default=".".join(socket.getfqdn().split(".")[1:]),
+        dest="sender_domain",
+        help=("domain name to use for sender email " "(default: %default)"),
+    )
+    parser.add_argument(
+        "-m",
+        default="localhost",
+        dest="mailserver",
+        help="mailserver to use for relaying email",
+    )
+    parser.add_argument(
+        "-p",
+        default=socket.getservbyname("smtp"),
+        dest="port",
+        type=int,
+        help="port to connect to SMTP on (default: %default)",
+    )
+    parser.add_argument(
+        "-s",
+        dest="subject",
+        help="subject line in email to send",
+    )
+    parser.add_argument(
+        "-u",
+        default=os.getenv("USER"),
+        dest="sender_user",
+        help=("username to use when sending mail " "(default: %default)"),
+    )
+    parser.add_argument("recipients", nargs="+")
 
-    parser = optparse.OptionParser()
-    parser.add_option('-d', default='.'.join(socket.getfqdn().split('.')[1:]),
-                      dest='sender_domain',
-                      help=('domain name to use for sender email '
-                            '(default: %default)'),
-                      )
-    parser.add_option('-m', default='localhost',
-                      dest='mailserver',
-                      help='mailserver to use for relaying email',
-                      )
-    parser.add_option('-p', default=socket.getservbyname('smtp'),
-                      dest='port',
-                      type=int,
-                      help='port to connect to SMTP on (default: %default)',
-                      )
-    parser.add_option('-s',
-                      dest='subject',
-                      help='subject line in email to send',
-                      )
-    parser.add_option('-u', default=os.getenv('USER'),
-                      dest='sender_user',
-                      help=('username to use when sending mail '
-                            '(default: %default)'),
-                      )
+    args = parser.parse_args()
 
-    opts, recipients = parser.parse_args()
-    if not len(recipients):
-        parser.exit('you must supply at least one recipient')
-
-    password = os.getenv('PASSWORD') or getpass.getpass()
-    password = password.strip().encode('utf-8')
+    password = os.getenv("PASSWORD") or getpass.getpass()
+    password = password.strip().encode("utf-8")
 
     rlist = [sys.stdin]
 
@@ -126,12 +150,19 @@ def main():
         ready = select.select(rlist, [], [])
         msg = ready[0][0].read()
         if msg:
-            do_email(opts.mailserver, opts.port, opts.sender_user,
-                     opts.sender_domain, password, recipients, msg,
-                     opts.subject)
+            do_email(
+                args.mailserver,
+                args.port,
+                args.sender_user,
+                args.sender_domain,
+                password,
+                args.recipients,
+                msg,
+                args.subject,
+            )
         else:
             time.sleep(1)
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
