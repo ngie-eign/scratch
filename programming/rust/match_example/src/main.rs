003b74d8c65babec208248da8ffab1d75316d3c3
Enji Cooper <yaneurabeya@gmail.com>
2023-07-28 18:32:51 -0700

    Example: add a test which demos match control flow
    This is sufficiently different from the Pluralsight example that I am
    not claiming it's directly derived from the course.

---
+++ programming/rust/match_example/src/main.rs
@@ -0,0 +1,19 @@
+fn pick_needle(haystack: String, needle_num: usize) -> String {
+    let needle_in_haystack = haystack.chars().nth(needle_num);
+
+    return match needle_in_haystack {
+        Some(needle) => format!("Needle #{} found in haystack: '{}'", needle_num, needle),
+        None => format!("Needle #{} not in the haystack!", needle_num)
+    };
+}
+
+fn main() {
+    let haystack: String = String::from("This is a haystack");
+
+    let last_needle_in_haystack_s = pick_needle(haystack.clone(), haystack.len() - 1);
+    let needle_not_in_haystack_s = pick_needle(haystack.clone(), haystack.len());
+
+    println!("Haystack contents: \"{}\".", haystack);
+    println!("{}", last_needle_in_haystack_s);
+    println!("{}", needle_not_in_haystack_s);
+}
