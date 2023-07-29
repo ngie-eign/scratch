7b9ceb6cd77862c9d7923a44f86241e7d9c8ef08
Enji Cooper <yaneurabeya@gmail.com>
2023-07-28 19:30:56 -0700

    Loops examples: make them a bit more amusing/practical :)

--- programming/rust/loup_loup/src/main.rs
+++ programming/rust/loup_loup/src/main.rs
@@ -1,30 +1,58 @@
 fn main() {
-    let mut i = 0;
+    let mut i = -1;
 
     // A "loop" loop.
     loop {
-        if i == 0 {
-            println!("They tried to make me go to rehab... but I said...");
-            i += 1;
-            continue;
-        } else if i <= 3 {
-            println!("\"No..\".");
-        } else {
-            break;
-        }
         i += 1;
+        match i {
+            0 => { println!("They tried to make me go to rehab... but I said..."); },
+            1..=3 => { println!("\"No..\"."); },
+            _ => { break; }
+        }
     }
 
     // A "while" loop.
     i = 0;
 
     while i <= 3 {
-        println!("311: 🎵I'll be... here a while... ain't goin' nowhere!🎵");
+        println!("🎵I'll be... here a while... ain't goin' nowhere!🎵");
         i += 1;
     }
 
-    // A "for" loop.
+    // A exclusive range for loop.
     for _j in 0..1 {
         println!("For-loop and seven instructions ago!");
+    }
+
+    // A inclusive for loop.
+    for _j in 0..=0 {
+        println!("One more time!");
+    }
+    println!("🎉🎉🎉 We're gonna celebrate 🎉🎉🎉!");
+
+    let hbfs_lyrics = [
+        "WORK--IT",
+        "MAKE--IT",
+        "DO--IT",
+        "MAKES--US",
+    ];
+
+    for line in &hbfs_lyrics {
+        println!("{}", line);
+    }
+
+    for _ in 1..=8 {
+        println!("dun... DUN");
+    }
+
+    let hbfs_lyrics = [
+        "HARDER",
+        "BETTER",
+        "FASTER",
+        "STRONGER"
+    ];
+
+    for line in hbfs_lyrics.iter() {
+        println!("{}", line);
     }
 }
