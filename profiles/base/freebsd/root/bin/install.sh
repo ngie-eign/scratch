01d32670953d334383e9ac928a2b72839c37bf35
Enji Cooper <yaneurabeya@gmail.com>
2024-10-19 09:03:18 -0700

    install.sh: handle the mergemaster removal gracefully
    In the event mergemaster is present, run that command (this is being
    done for older versions of FreeBSD). If mergemaster is not present,
    call etcupdate instead with equivalent args/flags.

--- profiles/base/freebsd/root/bin/install.sh
+++ profiles/base/freebsd/root/bin/install.sh
@@ -5,7 +5,11 @@ set -ex
 : ${DESTDIR=/}
 export DESTDIR
 
-mergemaster -iU -p -m $PWD -D $DESTDIR
+if which mergemaster 2>/dev/null; then
+	mergemaster -iU -p -m $PWD -D $DESTDIR
+else
+	etcupdate -s $PWD -D $DESTDIR -p
+fi
 $(dirname $0)/installkernel.sh || exit
 make installworld
 etcupdate -s $PWD -D $DESTDIR -F
