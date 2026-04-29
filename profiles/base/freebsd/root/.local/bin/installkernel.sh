db45a8731147f04dd1f1092f922fa224284ffc80
Enji Cooper <yaneurabeya@gmail.com>
2026-04-28 23:29:32 -0700

    Clean up script
    - make shellcheck clean.
    - Remove vestiges of svn support.
    - Use `VCS_VERSION` instead of `git` when setting `DEFAULT_KERNCONF`.
    - Don't hardcode `GENERIC`; leverage Makefile.inc1's definition instead and
      pick the first `KERNCONF` in the list when setting `DEFAULT_KERNCONF`.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- profiles/base/freebsd/root/.local/bin/installkernel.sh
+++ profiles/base/freebsd/root/.local/bin/installkernel.sh
@@ -2,50 +2,47 @@
 
 set -x
 
-: ${SYSDIR=$PWD/sys}
+: "${SYSDIR=${PWD}/sys}"
+SRCTOP="$(dirname "$(realpath "${SYSDIR}")")"
 
-if [ -d "${SYSDIR}/../.git" ] ; then
-	for dir in /usr/bin /usr/local/bin; do
-		if [ -x "${dir}/git" ] ; then
-			git_cmd="${dir}/git --git-dir=${SYSDIR}/../.git"
-			break
-		fi
-	done
+if [ -e "${SRCTOP}/.git" ] ; then
+	if _git="$(which git)"; then
+		git_cmd="${_git}"
+	fi
 fi
 
-if [ -n "$git_cmd" ] ; then
-	git=`$git_cmd rev-parse --verify --short HEAD 2>/dev/null`
-	git_b=`$git_cmd rev-parse --abbrev-ref HEAD`
-	if [ -n "$git_b" ] ; then
-		git="${git}"
+if [ -n "${git_cmd}" ] ; then
+	git=$("${git_cmd}" rev-parse --verify --short HEAD 2>/dev/null)
+	git_b="$("${git_cmd}" rev-parse --abbrev-ref HEAD)"
+	if [ -n "${git_b}" ] ; then
+		git="${git_b}"
 	fi
-	if $git_cmd --work-tree=${SYSDIR}/.. diff-index \
-	    --name-only HEAD | read dummy; then
+	if "${git_cmd}" diff-index --name-only HEAD | read -r _dummy; then
 		git="${git}-dirty"
 	fi
 fi
-git=$(echo -n "${git:-}" | sed -e 's, ,,g')
+git="$(printf "%s" "${git:-}" | tr -d '[:space:]')"
 
-: ${DESTDIR=/}
+: "${DESTDIR=/}"
 export DESTDIR
-: ${SRCCONF=/etc/src.conf}
+: "${SRCCONF=/etc/src.conf}"
 
-KERNCONF=$(make -VKERNCONF -f $SRCCONF)
-KERNCONF=${KERNCONF:-GENERIC}
-VCS_VERSION="${git:-$git}"
-[ -n "$VCS_VERSION" ] && VCS_VERSION=".${VCS_VERSION}"
+KERNCONFS="$(make -VKERNCONF -f "${SRCCONF}")"
+: "${DEFAULT_KERNCONF="$(make -V'KERNCONF:[0]')"}"
+VCS_VERSION="${git}"
+[ -n "${VCS_VERSION}" ] && VCS_VERSION=".${VCS_VERSION}"
 set -e
-for _kc in ${KERNCONF}; do
-	instkernname=$_kc${VCS_VERSION}
-	make installkernel INSTKERNNAME="$instkernname" KERNCONF=$_kc
+for _kc in ${KERNCONFS}; do
+	instkernname="${_kc}${VCS_VERSION}"
+	make installkernel INSTKERNNAME="${instkernname}" KERNCONF="${_kc}"
 done
 if [ -n "${DEFAULT_KERNCONF}" ]; then
-	default_kern_name=${DEFAULT_KERNCONF}.${svn:-$svn}${git:-$git}
+	default_kern_name="${DEFAULT_KERNCONF}${VCS_VERSION}"
 
 	if [ ! -d "${DESTDIR}/boot/${default_kern_name}" ]; then
 		echo "${default_kern_name} not installed at ${DESTDIR}/boot/"
 		exit 1
 	fi
 
-	ln -sfh ${default_kern_name} ${DESTDIR}/boot/kernel
+	ln -sfh "${default_kern_name}" "${DESTDIR}/boot/kernel"
 fi
